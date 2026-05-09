2ad623c703f65751f7c2c41c558534c3e17f6be7
AlBaash247 <138034243+AlBaash247@users.noreply.github.com>
2026-05-09 22:52:10 +0400

    # implement new style

--- js/templates/nav-bar.js
+++ js/templates/nav-bar.js
@@ -14,38 +14,47 @@ let defaultNavLinks = [
 
 
 
-
+let defaultAuthLinks = []
 
 
 createNavbarItems(defaultNavLinks);
 
 export function createNavbarItems(navLinks = defaultNavLinks) {
-    let navContainer = document.getElementById("navContainer")
+    let navLinksContainer = document.getElementById("navLinksContainer")
+    let navAuthLinksContainer = document.getElementById("navAuthLinksContainer")
 
     
     if (getToken() !== null ) {
-        defaultNavLinks.push({
+        defaultAuthLinks.push({
             label: 'Profile',
             link: path + 'profile.html'
         })
     }
     else {
-        defaultNavLinks.push({
+        defaultAuthLinks.push({
             label: 'Login',
             link: path + 'login.html'
         })
-        defaultNavLinks.push({
+        defaultAuthLinks.push({
             label: 'Register',
             link: path + 'register.html'
         })
     }
 
 
-    if (navContainer) {
+    if (navLinksContainer) {
 
         navLinks.forEach(item => {
             let linkElement = `<a href="${item.link}" class="nav-link">${item.label}</a>`
-            navContainer.innerHTML += linkElement
+            navLinksContainer.innerHTML += linkElement
+        });
+    }
+
+    if (navAuthLinksContainer) {
+
+        defaultAuthLinks.forEach(item => {
+            let linkElement = `<a href="${item.link}" class="nav-link">${item.label}</a>`
+            navAuthLinksContainer.innerHTML += linkElement
         });
     }
 
