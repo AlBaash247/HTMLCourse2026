58fdf71880c8b7cd1b9ea1483edb32279cf95cc7
AlBaash247 <138034243+AlBaash247@users.noreply.github.com>
2026-05-16 22:49:19 +0400

    # add modal

--- js/scripts/index.js
+++ js/scripts/index.js
@@ -11,8 +11,7 @@ import {
 let taskCategories = []
 
 const mainTasksColumnsContainer = document.getElementById('mainTasksColumnsContainer')
-const modalHeader = document.getElementById("modalHeader")
-const modalClose = document.getElementById("modalClose")
+const showTaskModalHeader = document.getElementById("showTaskModalHeader")
 
 
 
@@ -22,19 +21,28 @@ let taskColumnContainerTemplate = document.getElementById('taskColumnContainerTe
 let taskCardTemplate = document.getElementById('taskCardTemplate')
 
 
-// Get the modal
+// Get the show modal
 var showTaskModal = document.getElementById("showTaskModal");
-var modalTitle = document.getElementById("modalTitle");
-var modalDesc = document.getElementById("modalDesc");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+const showTaskModalClose = document.getElementById("showTaskModalClose")
+var showTaskModalTitle = document.getElementById("showTaskModalTitle");
+var showTaskModalDesc = document.getElementById("showTaskModalDesc");
 
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function () {
+// Get the add modal
+var addTaskModal = document.getElementById("addTaskModal");
+const addTaskModalClose = document.getElementById("addTaskModalClose")
+
+var addTaskModalTitle = document.getElementById("addTaskModalTitle");
+var addTaskModalDesc = document.getElementById("addTaskModalDesc");
+var closeShowModal = document.getElementsByClassName("close")[0];
+
+
+
+// When the user clicks on <closeShowModal> (x), close the modal
+showTaskModalClose.onclick = function () {
     showTaskModal.style.display = "none";
 }
+
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
@@ -43,6 +51,9 @@ window.onclick = function (event) {
     }
 }
 
+addTaskModalClose.onclick = function () {
+    addTaskModal.style.display = "none";
+}
 
 if (getToken()) {
     fetchTaskCategories()
@@ -68,88 +79,71 @@ async function fetchQuickTasks() {
     let response = await fetchApiData(METHOD_GET, API_URL_QUICK_TASK_INDEX, null)
 
     if (response.success) {
-        adapter(response.data)
+        columnsAdapter(response.data)
     }
 
 }
 
-function adapter(tasks) {
+function columnsAdapter(tasks) {
 
     taskCategories.forEach(category => {
 
         const columnContainerClone = taskColumnContainerTemplate.content.cloneNode(true)
         const taskColumn = columnContainerClone.getElementById("taskColumn")
         const taskColumnTitle = columnContainerClone.getElementById("taskColumnTitle")
+        const taskColumnAddTask = columnContainerClone.getElementById("taskColumnAddTask")
         const taskColumnCardsContainer = columnContainerClone.getElementById("taskColumnCardsContainer")
-        const filteredTasks = tasks.filter(task => task.task_category_id === category.id);
 
         taskColumn.key = category.id
         taskColumn.dataset.category_id = category.id
         taskColumnTitle.innerText = category.name
 
+        taskColumnAddTask.onclick = () => {
+            addTaskModalTitle.innerText = `Add Task to ${category.name}`
 
-        filteredTasks.forEach(task => {
-            const taskCardClone = taskCardTemplate.content.cloneNode(true)
-            const taskCard = taskCardClone.getElementById("taskCard")
-            const taskCardTitle = taskCardClone.getElementById("taskCardTitle")
-            const taskCardDesc = taskCardClone.getElementById("taskCardDesc")
+            addTaskModalHeader.classList.remove(...addTaskModalHeader.classList);
+            addTaskModalTitle.classList.remove(...addTaskModalTitle.classList);
+            addTaskModalClose.classList.remove(...addTaskModalClose.classList);
 
-            taskCard.key = task.id
-            taskCard.dataset.task_id = task.id
-            taskCardTitle.innerText = task.name.slice(0, 10)
-            taskCardDesc.innerText = task.name
+            switch (category.name) {
+                case 'Log':
+                    addTaskModalTitle.classList.add('text-accent-light')
+                    addTaskModalClose.classList.add('text-accent-light', 'close')
+                    addTaskModalHeader.classList.add('modal-header', 'bg-primary')
+                    break
+                case 'Todo':
+                    addTaskModalTitle.classList.add('text-accent-light')
+                    addTaskModalClose.classList.add('text-accent-light', 'close')
+                    addTaskModalHeader.classList.add('modal-header', 'bg-tertiary')
+                    break
+                case 'In Progress':
+                    addTaskModalTitle.classList.add('text-accent-light')
+                    addTaskModalClose.classList.add('text-accent-light', 'close')
+                    addTaskModalHeader.classList.add('modal-header', 'bg-secondary')
+                    break
 
+                case 'Review':
 
-            taskCard.onclick = () => {
-                modalTitle.innerText = task.name.slice(0, 10)
-                modalDesc.innerText = task.name
-                showTaskModal.style.display = "block";
+                    addTaskModalTitle.classList.add('text-accent-dark')
+                    addTaskModalClose.classList.add('text-accent-dark', 'close')
+                    addTaskModalHeader.classList.add('modal-header', 'bg-accent-light', 'border-bottom-accent-dark')
+                    break
 
-                modalHeader.classList.remove(...modalHeader.classList);
-                modalTitle.classList.remove(...modalTitle.classList);
-                modalClose.classList.remove(...modalClose.classList);
+                case 'Done':
+                    addTaskModalTitle.classList.add('text-accent-light')
+                    addTaskModalClose.classList.add('text-accent-light', 'close')
+                    addTaskModalHeader.classList.add('modal-header', 'bg-accent-dark')
+                    break
 
-                switch (category.name) {
-                    case 'Log':
-                        modalTitle.classList.add('text-accent-light')
-                        modalClose.classList.add('text-accent-light', 'close')
-                        modalHeader.classList.add('modal-header', 'bg-primary')
-                        break
-                    case 'Todo':
-                        modalTitle.classList.add('text-accent-light')
-                        modalClose.classList.add('text-accent-light', 'close')
-                        modalHeader.classList.add('modal-header', 'bg-tertiary')
-                        break
-                    case 'In Progress':
-                        modalTitle.classList.add('text-accent-light')
-                        modalClose.classList.add('text-accent-light', 'close')
-                        modalHeader.classList.add('modal-header', 'bg-secondary')
-                        break
-
-                    case 'Review':
-
-                        modalTitle.classList.add('text-accent-dark')
-                        modalClose.classList.add('text-accent-dark', 'close')
-                        modalHeader.classList.add('modal-header', 'bg-accent-light', 'border-bottom-accent-dark')
-                        break
-
-                    case 'Done':
-                        modalTitle.classList.add('text-accent-light')
-                        modalClose.classList.add('text-accent-light', 'close')
-                        modalHeader.classList.add('modal-header', 'bg-accent-dark')
-                        break
-
-                    default:
-                        modalHeader.classList.add('modal-header', 'bg-primary')
-                }
-
+                default:
+                    addTaskModalHeader.classList.add('modal-header', 'bg-primary')
             }
 
 
-            taskColumnCardsContainer.appendChild(taskCardClone)
+            addTaskModal.style.display = "block";
+        }
 
-        });
-
+        cardsAdapter(category, tasks, taskColumnCardsContainer)
 
         if (category.name != 'Archive') {
             mainTasksColumnsContainer.appendChild(columnContainerClone);
@@ -159,7 +153,73 @@ function adapter(tasks) {
 
 }
 
+function cardsAdapter(category, tasks, taskColumnCardsContainer) {
 
+    const filteredTasks = tasks.filter(task => task.task_category_id === category.id);
+
+    filteredTasks.forEach(task => {
+        const taskCardClone = taskCardTemplate.content.cloneNode(true)
+        const taskCard = taskCardClone.getElementById("taskCard")
+        const taskCardTitle = taskCardClone.getElementById("taskCardTitle")
+        const taskCardDesc = taskCardClone.getElementById("taskCardDesc")
+
+        taskCard.key = task.id
+        taskCard.dataset.task_id = task.id
+        taskCardTitle.innerText = task.name.slice(0, 10)
+        taskCardDesc.innerText = task.name
+
+
+        taskCard.onclick = () => {
+            showTaskModalTitle.innerText = task.name.slice(0, 10)
+            showTaskModalDesc.innerText = task.name
+
+            showTaskModalHeader.classList.remove(...showTaskModalHeader.classList);
+            showTaskModalTitle.classList.remove(...showTaskModalTitle.classList);
+            showTaskModalClose.classList.remove(...showTaskModalClose.classList);
+
+            switch (category.name) {
+                case 'Log':
+                    showTaskModalTitle.classList.add('text-accent-light')
+                    showTaskModalClose.classList.add('text-accent-light', 'close')
+                    showTaskModalHeader.classList.add('modal-header', 'bg-primary')
+                    break
+                case 'Todo':
+                    showTaskModalTitle.classList.add('text-accent-light')
+                    showTaskModalClose.classList.add('text-accent-light', 'close')
+                    showTaskModalHeader.classList.add('modal-header', 'bg-tertiary')
+                    break
+                case 'In Progress':
+                    showTaskModalTitle.classList.add('text-accent-light')
+                    showTaskModalClose.classList.add('text-accent-light', 'close')
+                    showTaskModalHeader.classList.add('modal-header', 'bg-secondary')
+                    break
+
+                case 'Review':
+
+                    showTaskModalTitle.classList.add('text-accent-dark')
+                    showTaskModalClose.classList.add('text-accent-dark', 'close')
+                    showTaskModalHeader.classList.add('modal-header', 'bg-accent-light', 'border-bottom-accent-dark')
+                    break
+
+                case 'Done':
+                    showTaskModalTitle.classList.add('text-accent-light')
+                    showTaskModalClose.classList.add('text-accent-light', 'close')
+                    showTaskModalHeader.classList.add('modal-header', 'bg-accent-dark')
+                    break
+
+                default:
+                    showTaskModalHeader.classList.add('modal-header', 'bg-primary')
+            }
+
+        }
+
+        showTaskModal.style.display = "block";
+
+        taskColumnCardsContainer.appendChild(taskCardClone)
+
+    });
+
+}
 
 
 
