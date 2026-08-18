7c8689da7e145558b7c19af8697d615601549267
Albaash247 <albaash247@gmail.com>
2026-08-18 19:28:54 +0400

    # tasks api ping index and create 3

--- kite/js/scripts/tasks.js
+++ kite/js/scripts/tasks.js
@@ -1,12 +1,21 @@
 import {BASE_URL, get_header, METHOD_POST, METHOD_GET, STORAGE_KEY_TOKEN} from '../constants/api.js'
 
 
+let inputTaskName = document.getElementById("inputTaskName");
+let inputTaskDone = document.getElementById("inputTaskDone");
+let inputTaskDesc = document.getElementById("inputTaskDesc");
+let inputTaskCatId = document.getElementById("inputTaskCatId");
+
+
+
 let textResult = document.getElementById("textResult");
 let btnPing = document.getElementById("btnPing");
+let btnCreate = document.getElementById("btnCreate");
 let btnIndex = document.getElementById("btnIndex");
 
 
 btnPing.onclick = ApiRequestTasksPing
+btnCreate.onclick = ApiRequestTasksCreate
 btnIndex.onclick = ApiRequestTasksIndex
 
 
@@ -41,3 +50,31 @@ async function ApiRequestTasksIndex() {
 }
 
 
+
+async function ApiRequestTasksCreate(){
+
+    let isDone = inputTaskDone.checked? 1 : 0;
+
+  let data = {
+        name : inputTaskName.value,
+        done: isDone,
+        description: inputTaskDesc.value,
+        task_category_id: inputTaskCatId.value,
+    }
+
+
+    let response = await fetch(`${BASE_URL}/tasks`, get_header(METHOD_POST, data));
+
+       
+    console.log("response", response);
+        
+        if(!response.ok){
+            alert("what have you done!!!!!! "+response.status )
+            return;
+        }
+       
+        let jsonResponse = await response.json();
+        console.log("jsonResponse", jsonResponse);
+
+        textResult.innerHTML = JSON.stringify(jsonResponse)
+}
