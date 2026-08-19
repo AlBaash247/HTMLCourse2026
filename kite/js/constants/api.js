68dffeed00fcc680d2ac265250aa66c20229737f
Albaash247 <albaash247@gmail.com>
2026-08-19 17:59:52 +0400

    # tasks api ping index and create 4

--- kite/js/constants/api.js
+++ kite/js/constants/api.js
@@ -4,6 +4,8 @@ export const STORAGE_KEY_TOKEN = "TOKEN";
 
 export const METHOD_GET = "GET";
 export const METHOD_POST = "POST";
+export const METHOD_PUT = "PUT";
+export const METHOD_DELETE= "DELETE";
 
 
 export function get_header(method, data){
--- kite/js/scripts/tasks.js
+++ kite/js/scripts/tasks.js
@@ -1,6 +1,7 @@
-import {BASE_URL, get_header, METHOD_POST, METHOD_GET, STORAGE_KEY_TOKEN} from '../constants/api.js'
+import {BASE_URL, get_header, METHOD_POST, METHOD_GET, METHOD_PUT, STORAGE_KEY_TOKEN} from '../constants/api.js'
 
 
+let inputTaskId = document.getElementById("inputTaskId");
 let inputTaskName = document.getElementById("inputTaskName");
 let inputTaskDone = document.getElementById("inputTaskDone");
 let inputTaskDesc = document.getElementById("inputTaskDesc");
@@ -12,11 +13,13 @@ let textResult = document.getElementById("textResult");
 let btnPing = document.getElementById("btnPing");
 let btnCreate = document.getElementById("btnCreate");
 let btnIndex = document.getElementById("btnIndex");
+let btnUpdate = document.getElementById("btnUpdate");
 
 
 btnPing.onclick = ApiRequestTasksPing
 btnCreate.onclick = ApiRequestTasksCreate
 btnIndex.onclick = ApiRequestTasksIndex
+btnUpdate.onclick = ApiRequestTasksUpdate
 
 
 async function ApiRequestTasksPing() {
@@ -49,8 +52,6 @@ async function ApiRequestTasksIndex() {
     textResult.innerText = JSON.stringify(jsonResponse);
 }
 
-
-
 async function ApiRequestTasksCreate(){
 
     let isDone = inputTaskDone.checked? 1 : 0;
@@ -64,6 +65,35 @@ async function ApiRequestTasksCreate(){
 
 
     let response = await fetch(`${BASE_URL}/tasks`, get_header(METHOD_POST, data));
+
+       
+    console.log("response", response);
+        
+        if(!response.ok){
+            alert("what have you done!!!!!! "+response.status )
+            return;
+        }
+       
+        let jsonResponse = await response.json();
+        console.log("jsonResponse", jsonResponse);
+
+        textResult.innerHTML = JSON.stringify(jsonResponse)
+}
+
+
+async function ApiRequestTasksUpdate(){
+
+    let isDone = inputTaskDone.checked? 1 : 0;
+
+  let data = {
+        name : inputTaskName.value,
+        done: isDone,
+        description: inputTaskDesc.value,
+        task_category_id: inputTaskCatId.value,
+    }
+
+    let taskId = inputTaskId.value
+    let response = await fetch(`${BASE_URL}/tasks/${taskId}`, get_header(METHOD_PUT, data));
 
        
     console.log("response", response);
